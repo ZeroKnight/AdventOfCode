294fc4214bdc10617801a49db2593b10120c96c0
ZeroKnight <xzeroknightx@gmail.com>
2016-12-01 17:35:56 -0800

    2016 Day 1, Part 2 [Perl]

--- 2016/Day01/day01.pl
+++ 2016/Day01/day01.pl
@@ -6,22 +6,47 @@
 use strict;
 use warnings;
 
+# Part 1
 my $heading = 0;
 my @blocks = qw/0 0 0 0/; # North East South West
+
+# Part 2
+my %city = ( 0 => { 0 => 1 } );
+my ($x, $y) = (0, 0);
+my $foundHQ = 0;
 
 open my $fh, '<', './input' or die "$0: can't open './input' for reading: $!";
 foreach my $step (split(/, /, do { local $/; <$fh>; }))
 {
+  my $count = substr($step, 1);
   if (substr($step, 0, 1) eq 'R')
   {
-    $heading = 0 if ++$heading == 4; # West to North
+    $heading = ++$heading % 4
   }
   else
   {
-    $heading = 3 if --$heading == -1; # North to West
+    $heading = --$heading % 4
   }
-  $blocks[$heading] += substr($step, 1);
+  $blocks[$heading] += $count;
+
+  unless ($foundHQ) {
+    foreach (1..$count)
+    {
+      ++$y if $heading == 0;
+      ++$x if $heading == 1;
+      --$y if $heading == 2;
+      --$x if $heading == 3;
+
+      if (++$city{$x}{$y} == 2)
+      {
+        $foundHQ = 1;
+        last;
+      }
+    }
+  }
 }
 
 my $distance = abs($blocks[0] - $blocks[2]) + abs($blocks[1] - $blocks[3]);
-print "$distance\n";
+my $distance2 = abs($x + $y);
+print "Part 1: $distance\n";
+print "Part 2: $distance2 ($x,$y)\n";

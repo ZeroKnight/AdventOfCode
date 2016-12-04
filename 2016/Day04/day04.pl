a9b1cba6bd21b7c4a8437ead4026fb6044a57909
ZeroKnight <xzeroknightx@gmail.com>
2016-12-04 15:53:19 -0800

    2016 Day 4 Part 2 [Perl]

---
+++ 2016/Day04/day04.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/env perl
+
+# Advent of Code 2016: Day 4
+# http://adventofcode.com/2016/day/4
+
+use strict;
+use warnings;
+
+use Carp::Assert;
+use List::Util qw/sum/;
+
+my %rooms;
+my $target;
+open my $fh, '<', './input' or die "$0: can't open './input' for reading: $!";
+
+ROOM: while (my $line = <$fh>)
+{
+  chomp($line);
+  my %letters = ();
+  my ($name, $sectorid, $checksum) = $line =~ /^([a-z-]+)-(\d+)\[(\w+)\]$/;
+
+  foreach (split //, $name)
+  {
+    ++$letters{$_} unless /-/;
+  }
+
+  my @most_common = sort keys %letters;
+  @most_common = (sort { $letters{$b} <=> $letters{$a} } @most_common)[0..4];
+  foreach my $c (split //, $checksum)
+  {
+    next ROOM unless (grep { $_ eq $c } @most_common);
+  }
+  $rooms{$name} = $sectorid;
+}
+
+assert(rot(343, 'qzmt-zixmtkozy-ivhz') eq 'very encrypted name') if DEBUG;
+foreach my $room (keys %rooms)
+{
+  if (rot($rooms{$room}, $room) =~ /.*north.*pole.*object.*/)
+  {
+    $target = $rooms{$room};
+    last;
+  }
+}
+
+print "Part 1: ".(scalar keys %rooms)." valid rooms whose sector ids sum to ".sum(values %rooms)."\n";
+print "Part 2: Sector ID of 'North Pole Objects' room: $target\n";
+
+sub rot
+{
+  my ($shift, $str) = @_;
+  my $delta = $shift % 26;
+  my $digest;
+
+  foreach my $c (split //, $str)
+  {
+    if ($c eq '-')
+    {
+      $c = ' ';
+    }
+    else
+    {
+      my $shifted = ord($c) + $delta;
+      $c = $shifted > 122 ? chr(($shifted - 123) + 97) : chr($shifted);
+    }
+    $digest .= $c;
+  }
+  return $digest;
+}
